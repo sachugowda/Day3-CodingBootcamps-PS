af85ffc6c31b7af12ef3bf918a86991e236c13c1
Sachin DN <sachugowda@hotmail.com>
2021-02-12 14:27:14 +0530

    Create Problem_2.c

---
+++ Problem_2.c
@@ -0,0 +1,22 @@
+//You will be given a number 'N'. Your task is to calculate the sum of first N natural numbers(1 to N)
+//Example
+//input: 
+//4
+//output: 
+//10
+
+#include<stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    //Write your code here
+       
+       
+       
+       
+       
+    return 0;
+}
